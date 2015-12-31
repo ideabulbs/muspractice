0e2b1df4ad38c316224fc4d3695febe8495f1ed4
Andrei Matveyeu <andrei@ideabulbs.com>
2015-12-31 21:47:45 +0100

    basic hooks

---
+++ muspractice/hooks.post/002_stop_recorcing.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import os
+import signal
+import time
+import datetime
+import re
+import subprocess
+
+def main():
+    pid_file = 'recording.pid'
+    if os.path.exists(pid_file):
+        data = open(pid_file, 'r').read()
+        if data:
+            pid = int(data.strip())
+            print "Stopping recording in process %d" % pid
+            os.kill(pid, signal.SIGINT)
+        os.unlink(pid_file)
+    phrase_id = os.environ['PHRASE_ID']
+    int_phrase_id = phrase_id
+    phrase_id = int(phrase_id)
+    phrase_id = "%.6d" % phrase_id
+    phrase_name = os.environ['PHRASE_NAME']
+    ts = time.time()
+    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H%M%S')
+    safe_phrase_name = "".join([c for c in phrase_name if re.match(r'\w', c)])
+    filename = "%s__%s__%s.mp3" % (phrase_id, timestamp, safe_phrase_name)
+
+    title = "%s %s (%s)" % (int_phrase_id, phrase_name, timestamp)
+    cmd = 'id3v2 -t "%s" record.mp3' % title
+    popen = subprocess.Popen(cmd, shell=True)
+    popen.communicate()
+
+    cmd = "mv record.mp3 ~/archive_muspractice/%s" % filename
+    popen = subprocess.Popen(cmd, shell=True)
+    popen.communicate()
+
+        
+if __name__ == "__main__":
+    main()
---
+++ muspractice/hooks.pre/002_start_record.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import subprocess
+import os
+
+def main():
+    cmd = 'jack_capture -p system:playback_1 -p system:capture_3 -c 2 -mp3 -mp3q 0 -mp3b 128 -fn record.mp3'
+    popen = subprocess.Popen(cmd.split(), shell=False, preexec_fn=os.setsid, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print 'Recording started in process #%d' % popen.pid
+    with open('recording.pid', 'w') as out:
+        out.write('%d' % popen.pid)
+
+if __name__ == '__main__':
+    main()
