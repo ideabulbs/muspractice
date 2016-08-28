27849c0db4e0293e9e8363e040957cd20fdde245
Andrei Matveyeu <andrei@ideabulbs.com>
2016-08-28 10:14:56 +0200

    qiv changed to xpaint

--- muspractice/tools/SightRead.py
+++ muspractice/tools/SightRead.py
@@ -37,7 +37,7 @@ class SightRead(object):
                 self.wait()
                 self._main_thread.stop()
                                 
-        command = "qiv %s" % (self.filename)
+        command = "xpaint %s" % (self.filename)
         self._is_playing = True
         if self.duration:
             self._popen = subprocess.Popen(command.split(), shell=False,
